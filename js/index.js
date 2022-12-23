aea1daec1958f4bc4d2fa0d85db05d2bd19d1a57
Alvaro Racca <alvaroracca@gmail.com>
2022-12-23 15:13:38 -0300

    Proyecto One Terminado

---
+++ js/index.js
@@ -0,0 +1,67 @@
+function ponerFrase(frase){
+    document.getElementById("ver-section").innerHTML = `
+        <textarea id="output" class="output" rows="8" readonly></textarea>
+        <button id="bt" onclick="copiar()">Copiar</button>
+    `;
+}
+
+function validarFrase(frase) {
+    /* Expresión regular para encontrar mayúsculas y carácteres con acento */
+    let regex = /[A-ZÀ-ú]/;
+    return (!regex.test(frase) && frase != '');
+}
+
+function encriptar(frase){
+    /* var texto = document.querySelector('#texto-encriptar').value; */
+    var textoEncriptado = frase.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
+    return textoEncriptado;
+    /* document.querySelector('#texto-desencriptado').value = textoEncriptado;
+    document.querySelector('#texto-encriptar').value; */
+}
+
+/* var btencriptar = document.querySelector('#btEncriptar');
+btencriptar.onclick = encriptar; */
+
+function desencriptar(frase){
+    /* var texto = document.querySelector('#texto-encriptar').value; */
+    var textoEncriptado = frase.replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ai/gi, 'a').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
+    return textoEncriptado;
+    /* document.querySelector('#texto-desencriptado').value = textoEncriptado;
+    document.querySelector('#texto-encriptar').value; */
+}
+
+
+function encriptado(){
+    let frase = document.getElementById("texto-encriptar").value;
+    if(validarFrase(frase)){
+        ponerFrase(frase);
+    document.getElementById("output").value = encriptar(frase);
+    } else {
+        alert("su mensaje esté vacío o tal vez contiene mayúsculas y/o acentos");
+        document.getElementById("output").value = '';
+    }
+    
+}
+
+function desencriptado(){
+    let frase = document.getElementById("texto-encriptar").value;
+    if(validarFrase(frase)){
+        ponerFrase(frase);
+        document.getElementById("output").value = desencriptar(frase);
+    } else{
+        alert("su mensaje esté vacío o tal vez contiene mayúsculas y/o acentos");
+        document.getElementById("output").value = '';
+    }
+    
+}
+
+function copiar() {
+    /* Obtenemos el texto a copiar */
+    let copiedText = document.getElementById("output").value;
+
+    /* Lo copiamos al portapapeles */
+    navigator.clipboard.writeText(copiedText);
+}
+
+/* var btdesencriptar = document.querySelector('#btDesencriptar');
+btdesencriptar.onclick = desencriptar; */
